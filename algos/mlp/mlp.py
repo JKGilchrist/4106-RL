f859861f02cea5f6ee178eef907acc5d6a0c9e9a
QuinnF19 <qfryday10@hotmail.com>
2019-12-05 15:59:44 -0500

    Update mlp.py

--- algos/mlp/mlp.py
+++ algos/mlp/mlp.py
@@ -10,8 +10,7 @@ class multi_layer_perceptron:
         model = MLPClassifier(solver='lbfgs', alpha=1e-4, hidden_layer_sizes=(150, 150), random_state=1, max_iter=100, learning_rate_init=0.01, warm_start=True)
 
     def train(self, df_attributes, df_target, epochs=50):
-        i = 0
-        while i < range(1, epochs + 1):
+        for i in range(1, epochs + 1):
             self.fit(df_attributes, df_target)
             i+=1
 
